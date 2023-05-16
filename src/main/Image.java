c2f651fc77291e332cecb9b572c0b1f18ebe3c12
T2WIN <grandclaye49@gmail.com>
2023-05-16 15:37:14 +0200

    Creating the Image class with noising

--- src/main/Image.java
+++ src/main/Image.java
@@ -1,9 +1,37 @@
 package src.main;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Random;
 
 public class Image {
     private ArrayList<ArrayList<Integer>> matrix;
+    private int sigma;
+    
+    public Image(File file, int sigma) {
+        this.matrix = createMatrix(file);
+        this.sigma = sigma;
+    }
 
-    public 
+    public void noising() {
+        Random rand = new Random(123);
+        for (int i=0; i<this.matrix.size(); i++) {
+            for (int j = 0; j<this.matrix.get(0).size(); j++) {
+                int coef = this.matrix.get(i).get(j);
+                this.matrix.get(i).set(j, coef + floor(rand.nextGaussian()*sigma)); 
+            }
+        }
+    }
+
+    private int floor(double d) {
+        return 0;
+    }
+
+    public ArrayList<ArrayList<Integer>> getMatrix() {
+        return this.matrix;
+    }
+
+    public void setMatrix(ArrayList<ArrayList<Integer>> matrix) {
+        this.matrix = matrix;
+    }
 
 }
