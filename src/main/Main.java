7519d2522d9fd33e0e3fb6d620327b9c8664b3ff
octave38 <octave.ganzhorn@gmail.com>
2023-05-17 10:56:59 +0200

    adding error

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,16 @@ import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
-        Image image = new Image("src/main/lenaa.png", 2);
-        System.out.println(image.getMatrix());
-        BufferedImage newImage = image.createImageFromMatrix(image.getMatrix());
-        image.createfile(newImage);
+        // Image image = new Image("src/main/lenaa.png", 2);
+        // System.out.println(image.getMatrix());
+        // BufferedImage newImage = image.createImageFromMatrix(image.getMatrix());
+        // image.createfile(newImage);
+
+        Error error = new Error("src/main/lenaa.png", 20);
+        System.out.println(error.MeanSquaredError());
     }
+
+        
+        
+    
 }
--- src/main/main.java
+++
@@ -1,8 +0,0 @@
-package src.main;
-
-public class main {
-    public static void main(String[] args) {
-        //
-        float res = MeanSquaredError( Image test1.jpg, Image lenaa.png);
-    }
-}
