8839d3a46512dc9b43eca9a83fd8f4fc8e7b4598
T2WIN <grandclaye49@gmail.com>
2023-05-17 10:38:07 +0200

    Fixing the noise function

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,10 @@ import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
-        Image image = new Image("src/main/lenaa.png", 2);
+        Image image = new Image("src/main/lenaa.png", 30);
         System.out.println(image.getMatrix());
+        System.out.println(image.getMatrix()[0][0]);
+        image.noising();
         BufferedImage newImage = image.createImageFromMatrix(image.getMatrix());
         image.createfile(newImage);
     }
