e5492b09f5765b4ae22435945a40454fbe17d7af
T2WIN <grandclaye49@gmail.com>
2023-05-17 15:36:54 +0200

    e

--- src/main/Patch.java
+++ src/main/Patch.java
@@ -23,8 +23,14 @@ public class Patch {
 
     public int[][] intoMatrix(int[] vector) {
         int i = 0;
+        int [][] matrix = new int[this.matrix.length][this.matrix[0].length];
         while (i < vector.length) {
-            
+            int c = i/this.matrix[0].length;
+            int l = i - c*this.matrix[0].length;
+            matrix[l][c] = vector[i];
+            System.out.println(l + "," + c);
+            i++;
         }
+        return matrix;
     }
 }
