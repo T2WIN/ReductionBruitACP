634a0632cae63d0f98984061bcc9b9f8af784524
octave38 <octave.ganzhorn@gmail.com>
2023-05-22 14:24:47 +0200

    local

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,9 +7,9 @@ public class Main {
         // System.out.println(image.getMatrix());
         // BufferedImage newImage = image.createImageFromMatrix(image.getMatrix());
         // image.createfile(newImage);
-
+        /* 
         Error error = new Error("src/main/lenaa.png", 20);
-        System.out.println(error.MeanSquaredError());
+        System.out.println(error.MeanSquaredError());*/
 
         
     }    
--- src/main/octave.java
+++ src/main/octave.java
@@ -7,33 +7,50 @@ import java.util.List;
 public class octave {
     public static List<Image> DecoupeImage(Image X, int W, int n){
 
-        int imageWidth = X.getWidth();
-        int imageHeight = X.getheight();
-        int numCols = imageWidth / W;
-        int numRows = imageHeight / W;
-
-        if (numCols * numRows < n){
-            throw new IllegalArgumentException("Nombre d'imagettes demandé supérieurau nombre d'imagettes disponibles.");
-        }
-
         List<Image> imagettes = new ArrayList<>();
+        List<int[][]> imagettesPositions = new ArrayList<>();
 
+        int[][] Tab = X.getMatrix();
 
-        for (int i = 0 ; i < n ; i++ ){
-            int colIndex = i % numCols;
-            int rowIndex = i / numCols;
-            
-            WritableImage imagette=new WritableImage(X.getPixelReader(), colIndex * W, rowIndex * W, W, W);
-            
+        int imagettesEnLargeur = Tab.length / W;
+        int imagettesEnHauteur = Tab[0].length / W;
 
+        int nombreTotalImagettes = imagettesEnLargeur * imagettesEnHauteur;
 
-
-
-
-
-
-
-
+        if (nombreTotalImagettes > n){
+            throw new IllegalArgumentException("Nombre d'imagettes demandé supérieurau nombre d'imagettes disponibles.");
+            return imagettes;
         }
-    }       
+
+        for ( int i = 0 ; i < imagettesEnHauteur ; i++ ){
+            for ( int j = 0 ; j < imagettesEnLargeur ; j++){
+                int x = i * W;
+                int y = j * W;
+
+                Image imagette = getSubimage(x,y,W,X);
+
+                imagettes.add(imagette);
+                int[][] position =new int[x][y];
+                imagettesPositions.add(position);
+            }
+        }
+        return imagettes;
+    }  
+    
+    public static Image getSubImage(int x, int y, int W, Image X){
+
+        int[][] Tab = createMatrix(X);
+        int[][] imagettesMatrice;
+        Image imagettes;
+
+        for ( int i = 0 ; i < W ; i++){
+            for ( int j = 0 ; j < W ; j++){
+                
+                imagettesMatrice[i][j] = Tab[x+i][y+j];
+            }
+        imagettes = createImageFromMatrix(Tab);
+        return(imagettes);
+    }
+
+    }
 }
