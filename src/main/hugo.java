56b81c0637a23dc1fd9cb790926e44fab83ca294
ErmeHugo <sembinelli@cy-tech.fr>
2023-05-22 10:18:11 +0200

    i

--- src/main/hugo.java
+++ src/main/hugo.java
@@ -59,6 +59,45 @@ public class hugo{
 
         // Reconstruction Patch
         
+        int [][] test2 = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                test2[i][j]=0;
+            }
+        }
+
+        int cointx;
+        int cointy;
+
+        // Ajout des patchs dans la matrice 
+        for (int k = 0; k < ListePatch.size(); k++) {
+            for (int l=0; l<s;s++){
+                for (int m=0; m<s; m++){
+                    cointx = ListePatch.get(k).positionX;
+                    cointy = ListePatch.get(k).positionY;
+                    int [][] patch = new int[s][s];
+                    patch = ListePatch.get(k).matrix;
+                    test2[cointx+l][cointy+m]= patch[l][m];
+                }
+            }
+        }
+        int x=5;
+        int y=5;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                // Test Coin
+                if ((i == 0 || i == x - 1) && (j == 0 || j == y - 1)){
+                    //
+                }
+                else if (i == 0 || i == s - 1 || j == 0 || j == s - 1){
+                    test2[i][j]= test2[i][j]/2;
+                }
+                else {
+                    test2[i][j]= test2[i][j]/4;
+                }
+                
+            }
+        }
     }
 }
 
