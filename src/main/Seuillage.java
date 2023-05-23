262caa54a3e6f6c180afe1c03227a91fe9afa28a
Sarahchaab <sarahchaab007@gmail.com>
2023-05-23 09:26:37 +0200

    e

--- src/main/Seuillage.java
+++ src/main/Seuillage.java
@@ -1,13 +1,19 @@
 package src.main;
 
-
-
 public class Seuillage {
 
     private double visuShrink;
     private double bayesShrink;
     private Image image;
 
+// constructeur de seuillage
+    public Seuillage(Image image) {
+        this.image = image;
+        this.bayesShrink = seuilB();
+        this.visuShrink = seuilV();
+    }
+
+// getter et setter de visuShrink
     public double getVisuShrink() {
         return this.visuShrink;
     }
@@ -17,6 +23,7 @@ public class Seuillage {
         this.visuShrink = visuShrink;
     }
 
+// getter et setter de bayesShrink
     public double getBayesShrink() {
         return this.bayesShrink;
     }
@@ -24,36 +31,43 @@ public class Seuillage {
      public void setBayesShrink(Integer bayesShrink) {
         this.bayesShrink = bayesShrink;
     }
-    public int HardThresholding(int threshold, int alpha) {
 
+// seuillage dur avec en paramètre threshold le seuil et alpha le coefficient
+    public double HardThresholding(double threshold, double alpha) {
+        System.out.println(threshold + "," + alpha);
         if (threshold >= Math.abs(alpha)) {
-            return 0;
+            return 0;   // met à 0 les coefficients en dessous du seuil
         }
         else {
-            return alpha;
+            return alpha; //ne modifie pas les coefficients au dessus du seuil
         }
     }
 
-    public int SoftThresholding(int threshold, int alpha) {
+//seuillage doux avec en paramètre threshold le seuil et alpha le coefficient
+    public double SoftThresholding(double threshold, double alpha) {
 
         if (threshold >= Math.abs(alpha)) {
-            return 0;
+            return 0;  // met à 0 les coefficients en dessous du seuil
         }
         if (threshold < alpha) {
-            return (alpha - threshold);
+            return (alpha - threshold); // retourne le coefficient moins le seuil si le seuil est strictement inférieur au coefficient
         }
         if ((-threshold) >= alpha) {
-            return (alpha + threshold);
+            return (alpha + threshold); // retourne le seuil plus le coefficient si le coefficient est inférieur au seuil qui lui est négatif
         }
 
         return 0;
     }
 
-    public int seuilV() {
-        int L = image.getMatrix().size()*image.getMatrix().get(0).size();
+
+    public double seuilV() {
+        int L = image.getMatrix().length*image.getMatrix()[0].length;
         visuShrink = image.getSigma()*Math.sqrt(2*Math.log(L));
-        return 0;
+        System.out.println(visuShrink);
+        return visuShrink;
     }
+
+//Fonction pour calculer le maximum entre deux nombres
     public double max(double a, double b) {
         if (a<=b){
             return b;
@@ -62,15 +76,17 @@ public class Seuillage {
             return a;
         }
     }
+
     // fonction pour calculer la moyenne d'un echantillon
     public double mean(int[] entiers) {
         double moy = 0;
         for (int i = 0 ; i < entiers.length ; i++) {
             moy = moy + entiers[i];
-        } 
+        }
         return (moy/entiers.length);
     }
-    //fonction calculer la variance 
+
+    //fonction calculer la variance d'un echantillon
     public double calculVariance() {
         double moyenne;
         double somme = 0;
@@ -84,18 +100,16 @@ public class Seuillage {
         for (int i = 0 ; i < echantillon.length ; i++) {
             somme = somme + (echantillon[i] - moyenne)*(echantillon[i] - moyenne);
         }
-        return somme/(echantillon.length -1);
-
-        
-        
-      
+        return somme/(echantillon.length -1);      
     }
 
-    public int seuilB() {
+    public double seuilB() {
         double variance = calculVariance();
-        double ecartType = Math.sqrt(max((Math.pow(variance,2)-Math.pow(image.getSigma(),2)) , 0));
+        System.out.println(variance);
+        double ecartType = Math.sqrt(max(variance-Math.pow(image.getSigma(),2) , 0));
+
         bayesShrink = Math.pow(image.getSigma(),2)/ecartType;
-        return 0;
+        return bayesShrink;
     }
 
 
