1a10a6ceeb93bc2285659d907136057df99d2897
Sarahchaab <sarahchaab007@gmail.com>
2023-05-24 10:37:51 +0200

    e

--- src/main/java/src/main/java/Main.java
+++ src/main/java/src/main/java/Main.java
@@ -5,27 +5,94 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
+        
         int sigma = readConsole("Saisir un entier sigma pour ajouter à la photo un bruit gaussien : ");
         int taille = readConsole("donner la taille du patch que vous souhaitez : ");
         Image image = new Image("src/main/lenaa.png", sigma, taille);
+        Seuillage seuillage = new Seuillage(image);
         int choixMethode = chooseMethode();
 
         if (choixMethode == 1) {
             image.noising();
+            int[][] imageBruité = image.getNoisedMatrix();
+            int l=imageBruité.length;
+            int c=imageBruité[1].length;
             ArrayList<Patch> listePatch = image.extractionPatch(image.getNoisedMatrix());
             int[][] matricePatchs = image.vectorPatch(listePatch);
             ACP acp = new ACP(matricePatchs);
             acp.MoyCov();
             acp.DoACP();
             acp.Proj();
-            Seuillage seuillage = new Seuillage(image);
+         
 
             double[][] alpha = acp.getVcontrib();
-            for (int i = 0; i < alpha.length; i++) {
+            int choixSeuil = chooseSeuil();
+            int choixSeuillage = chooseSeuillage();
+
+            if (choixSeuil == 1) {
+
+                double  threshold = seuillage.getVisuShrink();
+
+                if (choixSeuillage == 1) {
+
+                    for (int i = 0; i < alpha.length; i++) {
+
+                        for (int j = 0; j < alpha.length; j++) {
+
+                        double hardThresholdingResult = seuillage.HardThresholding(threshold, alpha[i][j]);
+                        alpha[i][j] = hardThresholdingResult;
+
+                        }
+                    }
+                }
+                if (choixSeuillage == 2) {
+
+                    for (int i = 0; i < alpha.length; i++) {
+
+                        for (int j = 0; j < alpha.length; j++) {
+
+                        double softThresholdingResult = seuillage.SoftThresholding(threshold, alpha[i][j]);
+                        alpha[i][j] = softThresholdingResult;
+
+                        }
                 
+                    }
+                }
+            }
+
+            if (choixSeuil == 2) {
+
+                double  threshold = seuillage.getBayesShrink();
+
+                if (choixSeuillage == 1) {
+
+                    for (int i = 0; i < alpha.length; i++) {
+
+                        for (int j = 0; j < alpha.length; j++) {
+
+                        double hardThresholdingResult = seuillage.HardThresholding(threshold, alpha[i][j]);
+                        alpha[i][j] = hardThresholdingResult;
+
+                        }
+                    }
+                }
+                if (choixSeuillage == 2) {
+
+                    for (int i = 0; i < alpha.length; i++) {
+
+                        for (int j = 0; j < alpha.length; j++) {
+
+                        double softThresholdingResult = seuillage.SoftThresholding(threshold, alpha[i][j]);
+                        alpha[i][j] = softThresholdingResult;
+                        int[][] assemblerMatrice = image.assemblagePatch(listePatch, l, c);
+
+                
+                    }
+                }
             }
         }
+        
+    }
 
         // Error error = new Error("src/main/lenaa.png", 20);import java.awt.Color;
         // System.out.println(error.MeanSquaredError());
@@ -39,23 +106,21 @@ public class Main {
         // patch.intoMatrix(vect);
         // System.out.println(patch.matrix[0][0]);
 
-        Seuillage seuillage = new Seuillage(image);
+
+        // double  threshold = seuillage.getVisuShrink();
+        // double alpha = -150;
+        // double hardThresholdingResult = seuillage.HardThresholding(threshold, alpha);
+        // double softThresholdingResult = seuillage.SoftThresholding(threshold, alpha);
+
+        // System.out.println("Résultat du seuillage avec HardThresholding : " + hardThresholdingResult);
+        // System.out.println("Résultat du seuillage avec SoftThresholding : " + softThresholdingResult);
+
+        // double seuillageV = seuillage.seuilV();
+        // double seuillageB = seuillage.seuilB();
 
 
-        double  threshold = seuillage.getVisuShrink();
-        double alpha = -150;
-        double hardThresholdingResult = seuillage.HardThresholding(threshold, alpha);
-        double softThresholdingResult = seuillage.SoftThresholding(threshold, alpha);
-
-        System.out.println("Résultat du seuillage avec HardThresholding : " + hardThresholdingResult);
-        System.out.println("Résultat du seuillage avec SoftThresholding : " + softThresholdingResult);
-
-        double seuillageV = seuillage.seuilV();
-        double seuillageB = seuillage.seuilB();
-
-
-        System.out.println("Résultat du seuilV : " + seuillageV);
-        System.out.println("Résultat du seuilB : " + seuillageB);
+        // System.out.println("Résultat du seuilV : " + seuillageV);
+        // System.out.println("Résultat du seuilB : " + seuillageB);
 
 
     }    
@@ -89,6 +154,47 @@ public class Main {
         else {
             System.out.println("Vous n'avez pas tapé un numéro correct pour le choix de la méthode de patchs. Recommencez s'il vous plaît.");
             chooseMethode();
+        }
+
+        return -1;
+
+    }
+    public static int chooseSeuil() {
+
+        int choixSeuil = readConsole("Souhaitez vous un seuil visuShrink ou un seuil bayesShrink ? pour le seuil visuShrink : tapez 1  pour le seuil bayesShrink : tapez 2");
+
+        if (choixSeuil == 1) {
+
+            return choixSeuil;
+        }
+        if (choixSeuil == 2) {
+
+            return choixSeuil;
+        }
+        else {
+            System.out.println("Vous n'avez pas tapé un numéro correct pour le choix du type de seuil. Recommencez s'il vous plaît.");
+            chooseSeuil();
+        }
+
+        return -1;
+
+    }
+
+    public static int chooseSeuillage() {
+
+        int choixSeuillage = readConsole("Souhaitez vous un seuillage dur ou un seuillage doux ? pour le seuillage dur : tapez 1  pour le seuillage doux : tapez 2");
+
+        if (choixSeuillage == 1) {
+
+            return choixSeuillage;
+        }
+        if (choixSeuillage == 2) {
+
+            return choixSeuillage;
+        }
+        else {
+            System.out.println("Vous n'avez pas tapé un numéro correct pour le choix du type de seuillage. Recommencez s'il vous plaît.");
+            chooseSeuillage();
         }
 
         return -1;
