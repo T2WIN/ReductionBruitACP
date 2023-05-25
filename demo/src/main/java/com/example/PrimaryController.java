5d8bd03b498f62b76e05bda8d9409cfe53fa318e
T2WIN <grandclaye49@gmail.com>
2023-05-25 09:34:48 +0200

    e

--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -2,11 +2,20 @@ package com.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 
 public class PrimaryController {
+    
+    @FXML
+    private ChoiceBox<String> choiceSigma;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void showValue() throws IOException {
+        System.out.println(choiceSigma.getItems());
     }
 }
