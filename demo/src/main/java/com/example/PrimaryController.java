973fed3fc4452269f2d1f055be075d9a0ffbfb8b
T2WIN <grandclaye49@gmail.com>
2023-05-26 09:49:41 +0200

    e

--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -3,11 +3,15 @@ package com.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 
 public class PrimaryController {
     
     @FXML
     private ChoiceBox<String> choiceSigma;
+    @FXML
+    private TextArea outputText;
+
     private Image image;
 
     @FXML
@@ -17,11 +21,7 @@ public class PrimaryController {
 
     @FXML
     private void fillImage() throws IOException {
-        this.image = new Image("", int sigma, int s);
-    }
-
-    @FXML
-    private void initialiseImage() {
-
+        this.image = new Image("demo/src/main/resources/com/example/lena.jpg", Integer.parseInt(choiceSigma.getValue()),  Integer.parseInt(outputText.getText()));
+        System.out.println(this.image.getSigma());
     }
 }
